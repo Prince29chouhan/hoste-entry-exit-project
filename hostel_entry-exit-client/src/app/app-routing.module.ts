d4474b06f6d5a91bd5167a9da693c298019c31ea
HariRags <harigovindraghunath@gmail.com>
2023-02-26 18:28:57 +0530

    dashboard layout done

--- hostel_entry-exit-client/src/app/app-routing.module.ts
+++ hostel_entry-exit-client/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashbComponent } from './dashb/dashb.component';
+
 import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
 import { LoginComponent } from './Login/login/login.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
-  {path:'dashboard', component:DashbComponent},
+  {path:'dashboard', component:DashboardComponent},
 ];
 
 @NgModule({
