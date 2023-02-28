28712257495336a36789f7a6a157222c1e795142
Prince29chouhan <princechouhan98765@gmail.com>
2023-03-01 00:06:58 +0530

    app routing updated

--- hostel_entry-exit-client/src/app/app-routing.module.ts
+++ hostel_entry-exit-client/src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
 import { EmptyComponent } from './empty/empty.component';
 import { LoginComponent } from './Login/login/login.component';
 import { NewcompComponent } from './newcomp/newcomp.component';
+import { HistoryComponent } from './History/history/history.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
   {path:'dashboard', component:DashboardComponent},
   {path:'newcomp', component:NewcompComponent},
   {path:'empty', component:EmptyComponent},
+  {path:'history', component:HistoryComponent},
+
 ];
 
 @NgModule({
