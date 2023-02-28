d1f3a5300dd909f436f7d249286fdb79c37f064a
Prince29chouhan <princechouhan98765@gmail.com>
2023-02-28 10:24:55 +0530

    dashboard and info page update

--- hostel_entry-exit-client/src/app/app-routing.module.ts
+++ hostel_entry-exit-client/src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
 import { LoginComponent } from './Login/login/login.component';
+import { NewcompComponent } from './newcomp/newcomp.component';
 
 const routes: Routes = [
   {path:'', component:LoginComponent},
   {path:'dashboard', component:DashboardComponent},
+  {path:'newcomp', component:NewcompComponent},
+
 ];
 
 @NgModule({
--- hostel_entry-exit-client/src/app/app.module.ts
+++ hostel_entry-exit-client/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './Dashboard/dashboard/dashboard.component';
 import { LoginComponent } from './Login/login/login.component';
 import { DashbComponent } from './dashb/dashb.component';
+import { NewcompComponent } from './newcomp/newcomp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     LoginComponent,
-    DashbComponent
+    DashbComponent,
+    NewcompComponent
   ],
   imports: [
     BrowserModule,
