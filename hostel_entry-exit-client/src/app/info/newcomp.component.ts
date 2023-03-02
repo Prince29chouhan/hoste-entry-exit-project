d282f33ca26c4af4b6e050cefd5c6dcfb2a8f1c2
Prince29chouhan <princechouhan98765@gmail.com>
2023-03-02 16:41:29 +0530

    newcomp: sendData fn added

--- hostel_entry-exit-client/src/app/info/newcomp.component.ts
+++ hostel_entry-exit-client/src/app/info/newcomp.component.ts
@@ -16,6 +16,11 @@ export class NewcompComponent {
     private http:HttpClient,
     private router:Router
   ){}
+  name_1:string='';
+  name_2:string='';
+  rollnum_1:string='';
+  rollnum_2:string='';
+  tempcall:tempdata= {} as tempdata;
       
   ngOnInit():void {
     //get dono users
@@ -32,7 +37,12 @@ export class NewcompComponent {
   }
 
   sendData(){
-    //callapi '/tempdata' store tempdata_object 
+    this.tempcall.name_1=this.name_1;
+  this.tempcall.name_2=this.name_2;
+  this.tempcall.rollnum_1=this.rollnum_1;
+  this.tempcall.rollnum_2=this.rollnum_2;
+
+  this.http.post<tempdata>('http://127.0.0.1:8000/tempdata/',this.tempcall).subscribe((result:any)=>{console.log(result);}) 
   }
  
 }
