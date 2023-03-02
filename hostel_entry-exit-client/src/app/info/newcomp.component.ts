0507bafd4de33afa4e6b28411afc275ad746572d
Vedant Thakre <44766005+vedant-github@users.noreply.github.com>
2023-03-02 18:59:26 +0530

    fixed:delayed api response and few other api calls

--- hostel_entry-exit-client/src/app/info/newcomp.component.ts
+++ hostel_entry-exit-client/src/app/info/newcomp.component.ts
@@ -16,34 +16,37 @@ export class NewcompComponent {
     private http:HttpClient,
     private router:Router
   ){}
-<<<<<<< HEAD
-  user1:user= {} as user; 
-  user2:user= {} as user; 
-=======
   name_1:string='';
   name_2:string='';
   rollnum_1:string='';
   rollnum_2:string='';
   tempcall:tempdata= {} as tempdata;
+
+  user1:user = {} as user
+  user2:user = {} as user
+  user3:user = {} as user
+
+  nodata:boolean=true
       
->>>>>>> d282f33ca26c4af4b6e050cefd5c6dcfb2a8f1c2
-  ngOnInit():void {
-    this.user1.name!=localStorage.getItem('user1.name')
-    this.user2.name!=localStorage.getItem('user2.name')
-    this.user1.roll_no!=localStorage.getItem('user1.roll_no')
-    this.user2.roll_no!=localStorage.getItem('user2.roll_no')
-    this.user1.hostel_no!=localStorage.getItem('user1.hostel_no')
-    this.user2.hostel_no!=localStorage.getItem('user2.hostel_no')
-    localStorage.removeItem('user1.name')
-    localStorage.removeItem('user2.name')
-    localStorage.removeItem('user1.roll_no')
-    localStorage.removeItem('user2.roll_no')
-    localStorage.removeItem('user1.hostel_no')
-    localStorage.removeItem('user2.hostel_no')
+  ngOnInit():void {    
+  }
+
+  getusers(){   
+    var hostel_visit = localStorage.getItem('h_num')
+
+    this.http.get<any>('http://127.0.0.1:8000/users/'+this.rollnum_1).subscribe((result:any)=>{ 
+    this.user1 = result
+  })
+  this.http.get<any>('http://127.0.0.1:8000/users/'+this.rollnum_2).subscribe(result=>{
+    this.user2 = result
+  }) 
+     
+  this.nodata =false
+  setTimeout(() => {
+    this.name_1 = this.user1.name
+  }, 1000);
+  
    
-    //localStorage.getItem()
-    //store in variables fir remove both from localstorage
-    //localStorage.removeItem
   }
 
   deleteKey(){
@@ -54,10 +57,12 @@ export class NewcompComponent {
   }
 
   sendData(){
-    this.tempcall.name_1=this.name_1;
+    console.log(this.name_1);
+  this.tempcall.name_1=this.name_1;
   this.tempcall.name_2=this.name_2;
   this.tempcall.rollnum_1=this.rollnum_1;
   this.tempcall.rollnum_2=this.rollnum_2;
+  this.tempcall.h_num=this.user1.hostel_no
 
   this.http.post<tempdata>('http://127.0.0.1:8000/tempdata/',this.tempcall).subscribe((result:any)=>{console.log(result);}) 
   }
